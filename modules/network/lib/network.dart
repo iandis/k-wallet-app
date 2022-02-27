16ec1a0fcd95d063d3e55d1dfdebdee68038df8b
Iandi Santulus <iandisantulusn@gmail.com>
2022-02-27 20:42:41 +0700

    feat: add network module

---
+++ modules/network/lib/network.dart
@@ -0,0 +1,7 @@
+library network;
+
+export 'src/gql_auth.dart';
+export 'src/gql_client.dart';
+export 'src/gql_executor.dart';
+export 'src/gql_parser.dart';
+export 'src/gql_type.dart';
---
+++ modules/network/lib/src/gql_executor.dart
@@ -0,0 +1,56 @@
+import 'package:graphql/client.dart';
+import 'package:meta/meta.dart';
+
+import 'gql_client.dart';
+import 'gql_parser.dart';
+import 'gql_type.dart';
+
+abstract class GQLExecutor<T> {
+  const GQLExecutor({
+    required GQLClient client,
+    required GQLParser<T> parser,
+  })  : _client = client,
+        _parser = parser;
+
+  final GQLClient _client;
+  final GQLParser<T> _parser;
+
+  FetchPolicy get _fetchPolicy => _parser.alwaysFetch
+      ? FetchPolicy.networkOnly
+      : FetchPolicy.cacheAndNetwork;
+
+  @nonVirtual
+  Future<T> execute(Map<String, dynamic> variables) async {
+    final QueryResult result;
+
+    if (_parser.type == GQLType.query) {
+      final QueryOptions queryOptions = QueryOptions(
+        document: gql(_parser.document),
+        variables: variables,
+        fetchPolicy: _fetchPolicy,
+      );
+
+      result = await _client.query(queryOptions);
+    } else {
+      final MutationOptions mutationOptions = MutationOptions(
+        document: gql(_parser.document),
+        variables: variables,
+        fetchPolicy: _fetchPolicy,
+      );
+
+      result = await _client.mutate(mutationOptions);
+    }
+
+    if (result.hasException) {
+      _parser.onError(result.exception!);
+    }
+
+    return _parser.parse(result.data!);
+  }
+
+  /// Calls [execute] without passing any variables.
+  @nonVirtual
+  Future<T> run() {
+    return execute(const <String, dynamic>{});
+  }
+}
